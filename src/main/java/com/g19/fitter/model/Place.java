15d18ebd4855fad197ce5d7e1e8cddba038f3ed3
David Sudia <dsudia@gmail.com>
2016-04-18 18:57:08 -0600

    created draft of place object

--- src/main/java/com/g19/fitter/model/Place.java
+++ src/main/java/com/g19/fitter/model/Place.java
@@ -1,7 +1,63 @@
 package com.g19.fitter.model;
 
+
+import java.util.ArrayList;
+
 /**
  * Created by davidsudia on 4/18/16.
  */
 public class Place {
+    private String url;
+    private String fullName;
+    private String countryCode;
+    private ArrayList<String> coordinates;
+    private String type;
+
+    public Place(String url, String fullName, String countryCode, ArrayList<String> coordinates, String type) {
+        this.url = url;
+        this.fullName = fullName;
+        this.countryCode = countryCode;
+        this.coordinates = coordinates;
+        this.type = type;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public ArrayList<String> getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(ArrayList<String> coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
