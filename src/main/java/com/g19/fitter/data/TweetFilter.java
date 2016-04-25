d1be3f9cbfa9bbbfe8cd6262a990d48036f38368
David Sudia <dsudia@gmail.com>
2016-04-25 15:44:53 -0600

    tweet service feeds filter which prints tweets

--- src/main/java/com/g19/fitter/data/TweetFilter.java
+++ src/main/java/com/g19/fitter/data/TweetFilter.java
@@ -1,7 +1,33 @@
 package com.g19.fitter.data;
 
+import org.springframework.social.twitter.api.Tweet;
+
+import java.util.concurrent.BlockingQueue;
+
 /**
  * Created by davidsudia on 4/15/16.
  */
-public class TweetFilter {
+public class TweetFilter implements Runnable {
+    private final BlockingQueue<Tweet> queue;
+
+    public TweetFilter(BlockingQueue<Tweet> queue) {
+        this.queue = queue;
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                Tweet tweet = queue.take();
+                System.out.println(tweet);
+                filterTweet(tweet);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void filterTweet(Tweet tweetEntity) {
+        System.out.println(tweetEntity);
+    }
+
 }
--- src/main/java/com/g19/fitter/data/TweetsService.java
+++ src/main/java/com/g19/fitter/data/TweetsService.java
@@ -1,49 +1,66 @@
 package com.g19.fitter.data;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.social.twitter.api.*;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.social.twitter.api.impl.TwitterTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 /**
  * Created by davidsudia on 4/18/16.
  */
 @Service
-public class TweetsService {
-    @Autowired
+public class TweetsService implements StreamListener {
+
+    @Inject
     private Twitter twitter;
+    @Inject
+    private ThreadPoolTaskExecutor taskExecutor;
+    @Value("${twitterProcessing.enabled}")
+    private boolean processingEnabled;
 
-    public List<Tweet> streamApi(String param1) {
-        final List<Tweet> tweets = new ArrayList<Tweet>();
+    private BlockingQueue<Tweet> queue = new ArrayBlockingQueue<Tweet>(20);
 
+    public void run() {
         List<StreamListener> listeners = new ArrayList<StreamListener>();
-        StreamListener streamListener = new StreamListener() {
-            public void onTweet(org.springframework.social.twitter.api.Tweet tweet) {
-                System.out.println(tweet);
-                tweets.add(tweet);
+        listeners.add(this);
+        twitter.streamingOperations().sample(listeners);
+    }
+
+    @PostConstruct
+    public void afterPropertiesSet() {
+        if (processingEnabled) {
+            for (int i = 0; i < taskExecutor.getMaxPoolSize(); i++) {
+                taskExecutor.execute(new TweetFilter(queue));
             }
+        }
 
-            public void onDelete(StreamDeleteEvent deleteEvent) {
-                System.out.println(deleteEvent);
-            }
+        run();
+    }
 
-            public void onLimit(int numberOfLimitedTweets) {
-                System.out.println(numberOfLimitedTweets);
-            }
+    public void onTweet(Tweet tweet) {
+        System.out.println("There was a tweet!");
+        queue.offer(tweet);
+    }
 
-            public void onWarning(StreamWarningEvent warningEvent) {
-                System.out.println(warningEvent);
-            }
-        };
+    public void onDelete(StreamDeleteEvent deleteEvent) {
+        System.out.println("I have just the thing for you! BALEETED!");
+    }
 
-        listeners.add(streamListener);
+    public void onLimit(int numberOfLimitedTweets) {
+        System.out.println("You've reached your limit! I'm cutting you off!");
+    }
 
-        Stream firehose = twitter.streamingOperations().firehose(listeners);
-
-        return tweets;
+    public void onWarning(StreamWarningEvent warningEvent) {
+        System.out.println("Warning! Danger Will Robinson!");
     }
 
 }
