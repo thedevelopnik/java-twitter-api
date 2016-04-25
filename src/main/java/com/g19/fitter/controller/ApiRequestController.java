23284344993fbeb5819cfc18486628509bbe810a
David Sudia <dsudia@gmail.com>
2016-04-25 15:46:02 -0600

    commented out unneeded controllers

--- src/main/java/com/g19/fitter/controller/ApiRequestController.java
+++ src/main/java/com/g19/fitter/controller/ApiRequestController.java
@@ -1,7 +1,33 @@
-package com.g19.fitter.controller;
-
-/**
- * Created by davidsudia on 4/22/16.
- */
-public class ApiRequestController {
-}
+//package com.g19.fitter.controller;
+//
+//import com.g19.fitter.data.TweetFilter;
+//import com.g19.fitter.model.Tweet;
+//import org.springframework.social.twitter.api.Twitter;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.ModelMap;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import com.g19.fitter.data.TweetsService;
+//
+//import javax.inject.Inject;
+//import java.util.concurrent.BlockingQueue;
+//
+///**
+// * Created by davidsudia on 4/22/16.
+// */
+//@Controller
+//public class ApiRequestController {
+//
+//    private final Twitter twitter;
+//
+//    @Inject
+//    public ApiRequestController(Twitter twitter) {
+//        this.twitter = twitter;
+//    }
+//
+//    @RequestMapping(value="/")
+//    public String streamTweet(ModelMap modelMap) {
+//        return "tweets";
+//    }
+//
+//}
+//
