53321519faba22c9c52e0983baafbf6f9920006a
David Sudia <dsudia@gmail.com>
2016-04-26 10:48:56 -0600

    deleted unnecessary imports

--- src/main/java/com/g19/fitter/data/SocketStart.java
+++ src/main/java/com/g19/fitter/data/SocketStart.java
@@ -1,11 +1,8 @@
 package com.g19.fitter.data;
 
-import com.g19.fitter.data.TweetsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.Twitter;
-import org.springframework.social.twitter.api.impl.TwitterTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 
 import javax.inject.Inject;
 
