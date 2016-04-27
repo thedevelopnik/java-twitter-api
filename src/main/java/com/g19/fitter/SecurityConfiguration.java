2fe408dc2dc77810601b0fc1a894232fa5104c68
David Sudia <dsudia@gmail.com>
2016-04-27 15:21:46 -0600

    setting up login/logout using Mongo

--- src/main/java/com/g19/fitter/SecurityConfiguration.java
+++ src/main/java/com/g19/fitter/SecurityConfiguration.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 /*
  * Created by davidsudia on 4/27/16.
  */
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity httpSecurity) throws Exception {
