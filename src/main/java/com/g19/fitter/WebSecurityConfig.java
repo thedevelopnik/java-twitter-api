f4707e56c0543c7e93db4c3ea8f6dae918ebd5af
David Sudia <dsudia@gmail.com>
2016-04-28 11:13:42 -0600

    cleaned up errors

--- src/main/java/com/g19/fitter/WebSecurityConfig.java
+++ src/main/java/com/g19/fitter/WebSecurityConfig.java
@@ -1,9 +1,7 @@
 package com.g19.fitter;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -39,7 +37,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        PasswordEncoder encoder = new BCryptPasswordEncoder();
-        return encoder;
+        return new BCryptPasswordEncoder();
     }
 }
