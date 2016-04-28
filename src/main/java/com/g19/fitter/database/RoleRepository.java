a61b6a0d5107efc722a48b91502b8f1c485d50e4
David Sudia <dsudia@gmail.com>
2016-04-28 11:17:13 -0600

    cleaned up warnings

--- src/main/java/com/g19/fitter/database/RoleRepository.java
+++ src/main/java/com/g19/fitter/database/RoleRepository.java
@@ -2,5 +2,5 @@ package com.g19.fitter.database;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface RoleRepository extends MongoRepository<Role, String> {
+interface RoleRepository extends MongoRepository<Role, String> {
 }
